31c19e27fd0b27965cb3f51d624070fd6521b511
smg0725 <smg0725@naver.com>
2024-08-10 22:51:10 +0900

    [SET] Member 도메인 셋팅

--- src/main/java/com/jo2/server/member/entity/Member.java
+++ src/main/java/com/jo2/server/member/entity/Member.java
@@ -6,11 +6,22 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import com.jo2.server.common.entity.BaseTime;
 import com.jo2.server.member.entity.enums.Gender;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member extends BaseTime {
 
     @Id
@@ -28,4 +39,21 @@ public class Member extends BaseTime {
     private Gender gender;
 
     private LocalDate birth;
+
+    public static Member of(
+            String email,
+            String password,
+            String name,
+            LocalDate birth,
+            Gender gender
+            ) {
+
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .birth(birth)
+                .gender(gender)
+                .build();
+    }
 }
