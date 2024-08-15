63d3510ba108ed5d3a59fbaa82ecaad94d1a6566
smg0725 <smg0725@naver.com>
2024-08-16 00:32:58 +0900

    [FEAT] Weather ErrorCode 구현

--- src/main/java/com/jo2/server/weather/message/ErrorCode.java
+++ src/main/java/com/jo2/server/weather/message/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    TEMP(HttpStatus.BAD_REQUEST, "임시 코드");
+    RANGE_EXCEPTION(HttpStatus.BAD_REQUEST, "점수의 범위는 1부터 100까지 입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
