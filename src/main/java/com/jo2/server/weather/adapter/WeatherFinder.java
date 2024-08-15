1520a42ec017b183142de814fd0777768c7d63f4
smg0725 <smg0725@naver.com>
2024-08-16 00:35:21 +0900

    [FEAT] WeatherFinder 구현

--- src/main/java/com/jo2/server/weather/adapter/WeatherFinder.java
+++ src/main/java/com/jo2/server/weather/adapter/WeatherFinder.java
@@ -1,11 +1,22 @@
 package com.jo2.server.weather.adapter;
 
 import com.jo2.server.common.support.RepositoryAdapter;
+import com.jo2.server.weather.entity.Weather;
+import com.jo2.server.weather.entity.WeatherList;
 import com.jo2.server.weather.repository.WeatherRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
 public class WeatherFinder {
     private final WeatherRepository weatherRepository;
+
+    public Optional<Weather> findTopByMemberIdOrderByCreatedAtDesc(long memberId){
+        return weatherRepository.findTopByMemberIdOrderByCreatedAtDesc(memberId);
+    }
+
+    public WeatherList findAllById(long memberId){
+        return WeatherList.from(weatherRepository.findAllById(memberId));
+    }
 }
