075fb8345da807b3337e3ec8819b7458af8c7138
smg0725 <smg0725@naver.com>
2024-08-16 00:35:47 +0900

    [FEAT] WeatherRepository 구현

--- src/main/java/com/jo2/server/weather/repository/WeatherRepository.java
+++ src/main/java/com/jo2/server/weather/repository/WeatherRepository.java
@@ -1,7 +1,17 @@
 package com.jo2.server.weather.repository;
 
 import com.jo2.server.weather.entity.Weather;
+import com.jo2.server.weather.entity.WeatherList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface WeatherRepository extends JpaRepository<Weather,Long> {
+public interface WeatherRepository extends JpaRepository<Weather, Long> {
+
+    @Query("SELECT w FROM Weather w WHERE w.member.id = :memberId ORDER BY w.date DESC, w.createdAt DESC")
+    Optional<Weather> findTopByMemberIdOrderByCreatedAtDesc(@Param("memberId") Long memberId);
+
+    List<Weather> findAllById(Long memberId);
 }
