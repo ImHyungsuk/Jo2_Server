7f4b748086208b723d7aea10bf3a3a5624a79ea0
smg0725 <smg0725@naver.com>
2024-08-16 00:35:09 +0900

    [FEAT] WeatherException 구현

--- src/main/java/com/jo2/server/weather/exception/WeatherException.java
+++ src/main/java/com/jo2/server/weather/exception/WeatherException.java
@@ -1,15 +1,15 @@
 package com.jo2.server.weather.exception;
 
-import com.jo2.server.chatting.message.ErrorCode;
+import com.jo2.server.weather.message.ErrorCode;
 import lombok.Getter;
 
 @Getter
-public class WeatherException extends RuntimeException{
+public class WeatherException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    public WeatherException(ErrorCode errorCode){
-        super("[WeatherException] : "+ errorCode.getMessage());
+    public WeatherException(ErrorCode errorCode) {
+        super("[WeatherException] : " + errorCode.getMessage());
         this.errorCode = errorCode;
     }
 }
