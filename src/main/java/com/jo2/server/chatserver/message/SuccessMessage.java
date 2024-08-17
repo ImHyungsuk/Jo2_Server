e9aa816f679a69b789eb3699fa97f7a639ff8fad
LimHyeongseok <limhs8630@gmail.com>
2024-08-18 01:52:52 +0900

    [FEAT] SuccessMessage 수정

--- src/main/java/com/jo2/server/chatserver/message/SuccessMessage.java
+++ src/main/java/com/jo2/server/chatserver/message/SuccessMessage.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessMessage {
     SUCCESS_START_CHATTING("새로운 채팅이 시작되었습니다."),
-    SUCCESS_END_CHATTING("채팅이 종료되었습니다.");
+    SUCCESS_ANALYSIS("분석이 완료되었습니다.");
 
     private final String message;
 }
