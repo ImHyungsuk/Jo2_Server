9dc09afddcd5e0def3613245bf34f3c22f469a9d
LimHyeongseok <limhs8630@gmail.com>
2024-08-17 18:41:41 +0900

    [FEAT] ChatserverService 구현

--- src/main/java/com/jo2/server/chatserver/service/ChatserverService.java
+++ src/main/java/com/jo2/server/chatserver/service/ChatserverService.java
@@ -1,5 +1,7 @@
 package com.jo2.server.chatserver.service;
 
+import com.jo2.server.chatserver.client.ChatserverClient;
+import com.jo2.server.chatserver.dto.response.ChatserverStartResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ChatserverService {
+    private final ChatserverClient chatserverClient;
+
+    public ChatserverStartResponse startChatserver(
+            final Long memberId
+    ) {
+        ChatserverStartResponse response = chatserverClient.startServer(memberId);
+        return response;
+    }
 }
