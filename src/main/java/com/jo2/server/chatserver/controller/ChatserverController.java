35a83dd2244fd9c4c1154fa4eb9b864e0c9ccf98
LimHyeongseok <limhs8630@gmail.com>
2024-08-17 18:42:15 +0900

    [FEAT] ChatserverController 구현

--- src/main/java/com/jo2/server/chatserver/controller/ChatserverController.java
+++ src/main/java/com/jo2/server/chatserver/controller/ChatserverController.java
@@ -1,9 +1,16 @@
 package com.jo2.server.chatserver.controller;
 
+import static com.jo2.server.common.dto.SuccessResponse.success;
+import static com.jo2.server.chatserver.message.SuccessMessage.SUCCESS_START_CHATTING;
+
+import com.jo2.server.chatserver.dto.response.ChatserverStartResponse;
 import com.jo2.server.chatserver.service.ChatserverService;
+import com.jo2.server.common.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatserverController {
 
     private final ChatserverService chatserverService;
+
+    @PostMapping
+    public ResponseEntity<SuccessResponse<?>> startChatting(Principal principal) {
+        Long memberId = Long.parseLong(principal.getName());
+        ChatserverStartResponse response = chatserverService.startChatserver(memberId);
+
+        return ResponseEntity.ok().body(success(SUCCESS_START_CHATTING.getMessage(), response));
+    }
 }
