16e3f0f09b1f2b6a48dfb305fc56718e1534d607
LimHyeongseok <limhs8630@gmail.com>
2024-08-18 22:20:31 +0900

    [FEAT] AnalysisRepository 구현

--- src/main/java/com/jo2/server/analysis/repository/AnalysisRepository.java
+++ src/main/java/com/jo2/server/analysis/repository/AnalysisRepository.java
@@ -4,4 +4,5 @@ import com.jo2.server.analysis.entity.Analysis;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
+
 }
