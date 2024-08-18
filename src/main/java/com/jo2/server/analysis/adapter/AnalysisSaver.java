c0e6760362ec3ac62a05b3ea30bcc0808e65f4eb
LimHyeongseok <limhs8630@gmail.com>
2024-08-18 22:19:59 +0900

    [FEAT] AnalysisSaver 구현

--- src/main/java/com/jo2/server/analysis/adapter/AnalysisSaver.java
+++ src/main/java/com/jo2/server/analysis/adapter/AnalysisSaver.java
@@ -1,5 +1,6 @@
 package com.jo2.server.analysis.adapter;
 
+import com.jo2.server.analysis.entity.Analysis;
 import com.jo2.server.analysis.repository.AnalysisRepository;
 import com.jo2.server.common.support.RepositoryAdapter;
 import lombok.RequiredArgsConstructor;
@@ -8,4 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AnalysisSaver {
     private final AnalysisRepository analysisRepository;
+
+    public void save(Analysis analysis) {
+        analysisRepository.save(analysis);
+    }
 }
