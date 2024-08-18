f43b21a88e17e2fe5f10fb7401082b26834e1958
LimHyeongseok <limhs8630@gmail.com>
2024-08-18 22:25:55 +0900

    [FEAT] MemberFinder 기능 추가

--- src/main/java/com/jo2/server/member/adapter/MemberFinder.java
+++ src/main/java/com/jo2/server/member/adapter/MemberFinder.java
@@ -1,12 +1,20 @@
 package com.jo2.server.member.adapter;
 
+import com.jo2.server.chatserver.config.OpenFeignConfig;
 import com.jo2.server.common.support.RepositoryAdapter;
+import com.jo2.server.member.entity.Member;
 import com.jo2.server.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @RepositoryAdapter
 @RequiredArgsConstructor
 public class MemberFinder {
 
     private final MemberRepository memberRepository;
+
+    public Optional<Member> findById(long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
