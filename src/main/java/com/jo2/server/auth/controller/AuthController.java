e9761c513d8595aebf1e2d0654be10e96bc0747d
smg0725 <smg0725@naver.com>
2024-08-18 20:51:13 +0900

    [FEAT] 소셜로그인 컨트롤러 서비스 구현

--- src/main/java/com/jo2/server/auth/controller/AuthController.java
+++ src/main/java/com/jo2/server/auth/controller/AuthController.java
@@ -1,8 +1,20 @@
 package com.jo2.server.auth.controller;
 
+import static com.jo2.server.auth.message.SuccessMessage.SUCCESS_SIGN_IN_INFOR;
+import static com.jo2.server.common.dto.SuccessResponse.success;
+import com.jo2.server.auth.dto.request.UserSignInRequest;
+import com.jo2.server.auth.dto.response.ReissueGetResponse;
+import com.jo2.server.auth.dto.response.SignInGetResponse;
 import com.jo2.server.auth.service.AuthService;
+import com.jo2.server.common.dto.SuccessResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    @PostMapping("/login")
+    public ResponseEntity<SuccessResponse<SignInGetResponse>> signIn(
+            @RequestParam final String authorizationCode,
+            @RequestBody final UserSignInRequest signInRequest,
+            HttpServletResponse httpServletResponse
+    ){
+        SignInGetResponse response = authService.create(authorizationCode,signInRequest);
+        return ResponseEntity.ok().body(success(SUCCESS_SIGN_IN_INFOR.getMessage(), response));
+    }
+
+    @GetMapping("/refresh-token")
+    public ResponseEntity<SuccessResponse<ReissueGetResponse>> refreshToken(
+            @RequestParam final String refreshToken
+    ) {
+        ReissueGetResponse response = authService.refreshToken(refreshToken);
+        return ResponseEntity.ok().body(success(SUCCESS_SIGN_IN_INFOR.getMessage(), response));
+    }
+
 }
--- src/main/java/com/jo2/server/auth/service/AuthService.java
+++ src/main/java/com/jo2/server/auth/service/AuthService.java
@@ -1,11 +1,81 @@
 package com.jo2.server.auth.service;
 
+import com.jo2.server.auth.dto.request.UserSignInRequest;
+import com.jo2.server.auth.dto.response.ReissueGetResponse;
+import com.jo2.server.auth.dto.response.SignInGetResponse;
+import com.jo2.server.auth.jwt.JwtTokenProvider;
+import com.jo2.server.auth.jwt.service.TokenService;
+import com.jo2.server.auth.social.SocialType;
+import com.jo2.server.auth.strategy.SignInStrategyManager;
+import com.jo2.server.auth.strategy.dto.UserInfoResponse;
+import com.jo2.server.member.adapter.MemberCreator;
+import com.jo2.server.member.adapter.MemberFinder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.dao.DataIntegrityViolationException;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuthService {
+
+    private final SignInStrategyManager loginStrategyManager;
+    private final MemberFinder memberFinder;
+    private final MemberCreator memberCreator;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final TokenService tokenService;
+
+    public SignInGetResponse create(
+            final String authorizationCode,
+            final UserSignInRequest signInRequest
+    ){
+        return getTokenDto(getUserInfoResponse(authorizationCode, signInRequest));
+    }
+    public ReissueGetResponse refreshToken(
+            final String refreshToken
+    ) {
+        final Long userId = tokenService.findIdByRefreshToken(refreshToken);
+
+        return ReissueGetResponse.from(
+                jwtTokenProvider.issueAccessToken(userId)
+        );
+    }
+
+    public UserInfoResponse getUserInfoResponse(
+            final String authorizationCode,
+            final UserSignInRequest signInRequest
+    ) {
+        return switch (signInRequest.socialType()) {
+            case KAKAO ->
+                    loginStrategyManager.getLoginStrategy(SocialType.KAKAO).login(authorizationCode, signInRequest);
+            case GOOGLE ->
+                    loginStrategyManager.getLoginStrategy(SocialType.GOOGLE).login(authorizationCode, signInRequest);
+        };
+    }
+
+    private SignInGetResponse getTokenDto(
+            final UserInfoResponse userResponse
+    ) {
+        try {
+            if (memberFinder.isExistingUser(userResponse.socialId(), userResponse.socialType())) {
+                return getTokenByUserId(memberFinder.getBySocialId(userResponse.socialId(), userResponse.socialType()).getId());
+            } else {
+                Long id = memberCreator.createMember(userResponse.socialId(), userResponse.socialType(), userResponse.email());
+                return getTokenByUserId(id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            return getTokenByUserId(memberFinder.getBySocialId(userResponse.socialId(), userResponse.socialType()).getId());
+        }
+    }
+
+    public SignInGetResponse getTokenByUserId(
+            final Long id
+    ) {
+        String refreshToken = jwtTokenProvider.issueRefreshToken(id);
+        tokenService.saveRefreshToken(id, refreshToken);
+        return SignInGetResponse.of(
+                jwtTokenProvider.issueAccessToken(id),
+                refreshToken
+        );
+    }
 }
