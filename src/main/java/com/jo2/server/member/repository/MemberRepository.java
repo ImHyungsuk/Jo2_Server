09ed733fb37408b3c3b88029d72bf8976d5e54e3
LimHyeongseok <limhs8630@gmail.com>
2024-08-18 22:25:28 +0900

    [FEAT] MemberRepository 기능 추가

--- src/main/java/com/jo2/server/member/repository/MemberRepository.java
+++ src/main/java/com/jo2/server/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.jo2.server.member.repository;
 import com.jo2.server.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findById(Long memberId);
 }
