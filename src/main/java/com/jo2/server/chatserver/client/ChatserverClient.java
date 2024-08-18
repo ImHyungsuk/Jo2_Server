8f888f5be0b8355d8f3302946464bddb2225f7cc
LimHyeongseok <limhs8630@gmail.com>
2024-08-18 22:16:12 +0900

    [FEAT] ChatserverClient 구현

--- src/main/java/com/jo2/server/chatserver/client/ChatserverClient.java
+++ src/main/java/com/jo2/server/chatserver/client/ChatserverClient.java
@@ -1,9 +1,13 @@
 package com.jo2.server.chatserver.client;
 
+import com.jo2.server.chatserver.dto.response.ChatserverAnalysisResponse;
 import com.jo2.server.chatserver.dto.response.ChatserverStartResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient(name = "chatserverClient",url = "http://localhost:5000/chatbot")
 public interface ChatserverClient {
@@ -11,5 +15,11 @@ public interface ChatserverClient {
     @PostMapping(value = "/start")
     ChatserverStartResponse startServer(
             @RequestParam("user_id") Long memberId
+    );
+
+    @PostMapping(value = "/analyze")
+    ChatserverAnalysisResponse analysis(
+            @RequestParam("user_id") Long memberId,
+            @RequestParam("summaries") List<String> summaries
     );
 }
