310315265dd7e6999ddc40ca53d7c4b0855752ae
paragon0107 <smg0725@naver.com>
2024-08-19 20:19:28 +0900

    [CHORE] CorsConfig 수정

--- src/main/java/com/jo2/server/common/config/CorsConfig.java
+++ src/main/java/com/jo2/server/common/config/CorsConfig.java
@@ -1,10 +1,19 @@
 package com.jo2.server.common.config;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@RequiredArgsConstructor
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
