c0bd98c84ffe96a111989ae9049c8e6a975524ac
smg0725 <smg0725@naver.com>
2024-08-23 19:28:25 +0900

    [CHORE] 로그인 시 닉네임 반환 서비스 수정

--- src/main/java/com/jo2/server/auth/service/AuthService.java
+++ src/main/java/com/jo2/server/auth/service/AuthService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.dao.DataIntegrityViolationException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -29,9 +30,10 @@ public class AuthService {
     public SignInGetResponse create(
             final String authorizationCode,
             final UserSignInRequest signInRequest
-    ){
+    ) {
         return getTokenDto(getUserInfoResponse(authorizationCode, signInRequest));
     }
+
     public ReissueGetResponse refreshToken(
             final String refreshToken
     ) {
@@ -57,26 +59,33 @@ public class AuthService {
     private SignInGetResponse getTokenDto(
             final UserInfoResponse userResponse
     ) {
+        String socialId = userResponse.socialId();
+        SocialType socialType = userResponse.socialType();
+        String nickname = userResponse.nickname();
         try {
-            if (memberFinder.isExistingUser(userResponse.socialId(), userResponse.socialType())) {
-                return getTokenByUserId(memberFinder.getBySocialId(userResponse.socialId(), userResponse.socialType()).getId());
+            if (memberFinder.isExistingUser(socialId, socialType)) {
+                return getTokenByUserId(
+                        memberFinder.getBySocialId(socialId, socialType).getId(), nickname);
             } else {
-                Long id = memberCreator.createMember(userResponse.socialId(), userResponse.socialType(), userResponse.email());
-                return getTokenByUserId(id);
+                Long id = memberCreator.createMember(socialId, socialType, nickname);
+                return getTokenByUserId(id, nickname);
             }
         } catch (DataIntegrityViolationException e) {
-            return getTokenByUserId(memberFinder.getBySocialId(userResponse.socialId(), userResponse.socialType()).getId());
+            return getTokenByUserId(
+                    memberFinder.getBySocialId(socialId, socialType).getId(), nickname);
         }
     }
 
     public SignInGetResponse getTokenByUserId(
-            final Long id
+            final Long id,
+            final String nickname
     ) {
         String refreshToken = jwtTokenProvider.issueRefreshToken(id);
         tokenService.saveRefreshToken(id, refreshToken);
         return SignInGetResponse.of(
                 jwtTokenProvider.issueAccessToken(id),
-                refreshToken
+                refreshToken,
+                nickname
         );
     }
 }
