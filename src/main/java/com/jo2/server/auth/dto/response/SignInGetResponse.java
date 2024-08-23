88fdf85c564875d16f6d3268d7e2812e57c2f5b2
smg0725 <smg0725@naver.com>
2024-08-23 19:29:16 +0900

    [CHORE] SignInGetResponse 닉네임 추가

--- src/main/java/com/jo2/server/auth/dto/response/SignInGetResponse.java
+++ src/main/java/com/jo2/server/auth/dto/response/SignInGetResponse.java
@@ -2,9 +2,10 @@ package com.jo2.server.auth.dto.response;
 
 public record SignInGetResponse(
         String accessToken,
-        String refreshToken
+        String refreshToken,
+        String nickname
 ) {
-    public static SignInGetResponse of(final String accessToken, final String refreshToken) {
-        return new SignInGetResponse(accessToken,refreshToken);
+    public static SignInGetResponse of(final String accessToken, final String refreshToken, String nickname) {
+        return new SignInGetResponse(accessToken, refreshToken, nickname);
     }
 }
