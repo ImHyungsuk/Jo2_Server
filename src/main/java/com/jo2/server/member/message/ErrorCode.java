879c2802c3e45e72d17599b969015246c70ec96f
smg0725 <smg0725@naver.com>
2024-08-26 15:57:24 +0900

    [CHORE] 에러 이름 수정

--- src/main/java/com/jo2/server/member/message/ErrorCode.java
+++ src/main/java/com/jo2/server/member/message/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버가 존재하지 않습니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
