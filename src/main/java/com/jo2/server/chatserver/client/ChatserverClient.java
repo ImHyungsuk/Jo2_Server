1c9b47c0f3967570be74a3d4d1a3ee08816e12db
smg0725 <smg0725@naver.com>
2024-08-26 15:57:14 +0900

    [CHORE] DTO 네이밍 수정

--- src/main/java/com/jo2/server/chatserver/client/ChatserverClient.java
+++ src/main/java/com/jo2/server/chatserver/client/ChatserverClient.java
@@ -1,14 +1,12 @@
 package com.jo2.server.chatserver.client;
 
-import com.jo2.server.chatserver.client.dto.ChatserverAnalysisRequest;
+import com.jo2.server.chatserver.client.dto.ChatServerAnalysisRequest;
 import com.jo2.server.chatserver.dto.request.ChatServerStartRequest;
 import com.jo2.server.chatserver.dto.response.ChatserverAnalysisResponse;
 import com.jo2.server.chatserver.dto.response.ChatserverStartResponse;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @FeignClient(name = "chatserverClient",url = "http://3.34.120.240:5000/api/chatbot")
@@ -21,7 +19,7 @@ public interface ChatserverClient {
 
     @PostMapping(value = "/analyze", consumes = "application/json")
     ChatserverAnalysisResponse analysis(
-            @RequestBody ChatserverAnalysisRequest chatserverAnalysisRequest
+            @RequestBody ChatServerAnalysisRequest chatserverAnalysisRequest
             );
 
 }
