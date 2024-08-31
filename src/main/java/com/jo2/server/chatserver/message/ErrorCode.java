fd896fe43da37755401963c17137b70d4179ea6e
smg0725 <smg0725@naver.com>
2024-09-01 05:16:00 +0900

    [CHORE] 에러 코드 수정

--- src/main/java/com/jo2/server/chatserver/message/ErrorCode.java
+++ src/main/java/com/jo2/server/chatserver/message/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    TEMP(HttpStatus.BAD_REQUEST, "임시 코드");
+    NONE_EXIST_DATA(HttpStatus.BAD_REQUEST, "검사 기록이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
