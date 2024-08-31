d09e0610fb9d340210c932779cc40d4e5917c0cd
smg0725 <smg0725@naver.com>
2024-09-01 05:09:29 +0900

    [CHORE] LocalDate 변환 메서드 추가

--- src/main/java/com/jo2/server/analysis/entity/Analysis.java
+++ src/main/java/com/jo2/server/analysis/entity/Analysis.java
@@ -3,13 +3,12 @@ package com.jo2.server.analysis.entity;
 import com.jo2.server.common.entity.BaseTime;
 import com.jo2.server.member.entity.Member;
 import jakarta.persistence.*;
+import java.time.LocalDate;
 import lombok.*;
 
-import java.time.LocalDate;
-
 @Entity
-@Getter
 @Builder
+@Getter
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Analysis extends BaseTime {
@@ -23,24 +22,23 @@ public class Analysis extends BaseTime {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private Long weatherId;
     private String result;
 
     public static Analysis of(
             Member member,
-            Long weatherId,
             String result
-    ){
+    ) {
         return Analysis.builder()
                 .member(member)
-                .weatherId(weatherId)
                 .result(result)
                 .build();
     }
 
-    public void updateResult(String result,Long weatherId)
-    {
+    public void updateResult(String result) {
         this.result = result;
-        this.weatherId = weatherId;
+    }
+
+    public LocalDate toLoCalDate() {
+        return updatedAt.toLocalDate();
     }
 }
