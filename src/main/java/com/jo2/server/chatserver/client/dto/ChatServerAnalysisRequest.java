ce580c2f5a741bc9f7da7b905198ed73b4540f65
smg0725 <smg0725@naver.com>
2024-09-01 05:14:38 +0900

    [CHORE] SnakeCase 적용

--- src/main/java/com/jo2/server/chatserver/client/dto/ChatServerAnalysisRequest.java
+++ src/main/java/com/jo2/server/chatserver/client/dto/ChatServerAnalysisRequest.java
@@ -1,12 +1,14 @@
 package com.jo2.server.chatserver.client.dto;
 
-import com.jo2.server.weather.entity.Weather;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.jo2.server.weather.vo.WeatherVO;
 import java.util.List;
-
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ChatServerAnalysisRequest(
-        List<WeatherAnalysisVO> weatherList
+        List<ChatServerAnalysisDataRequest> weatherList
 ) {
-    public static ChatServerAnalysisRequest from(List<Weather> weatherList) {
-        return new ChatServerAnalysisRequest(weatherList.stream().map(WeatherAnalysisVO::from).toList());
+    public static ChatServerAnalysisRequest from(List<WeatherVO> weatherList) {
+        return new ChatServerAnalysisRequest(weatherList.stream().map(ChatServerAnalysisDataRequest::from).toList());
     }
 }
