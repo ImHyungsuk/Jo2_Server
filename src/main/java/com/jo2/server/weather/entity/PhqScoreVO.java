ba0c1d28197919124463d41014bfe7950c3b4d1b
smg0725 <smg0725@naver.com>
2024-09-02 00:37:14 +0900

    [CHORE] 임시 커밋

--- src/main/java/com/jo2/server/weather/entity/PhqScoreVO.java
+++ src/main/java/com/jo2/server/weather/entity/PhqScoreVO.java
@@ -27,7 +27,7 @@ public class PhqScoreVO {
     }
 
     private void validateNumberIsInRange(int number) {
-        if (number <= MIN_NUMBER || number >= MAX_NUMBER) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new WeatherException(RANGE_EXCEPTION);
         }
     }
